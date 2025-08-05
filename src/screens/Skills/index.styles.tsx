ac79189d3842ade7395e87a6143fb3f4a4c80adc
Leandro Fonseca <leandromkt@outlook.com>
2025-08-05 16:15:27 -0300

    feat: create Skills screen

---
+++ src/screens/Skills/index.styles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container:{
+        width:'100%',
+        height: "100%",
+        backgroundColor: "#121315",
+        justifyContent: "space-evenly",
+        paddingVertical: 30,
+        paddingHorizontal: 24
+    },
+    backButtonContainer: {
+        marginBottom: -10
+    }
+});
--- src/screens/Skills/index.tsx
+++ src/screens/Skills/index.tsx
@@ -1,11 +1,61 @@
-import { Text, View } from "react-native"
+import { Pressable, View } from "react-native"
+import Header from "../../components/Header";
+import Button from "../../components/Button";
+import { SkillItemProps } from "../../components/SkillItem/index.types";
+import { styles } from "./index.styles";
+import { StatusBar } from "expo-status-bar";
+import SkillsList from "../../components/SkillsList";
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from "@react-navigation/native";
+import { NavigationProps } from "./index.types";
 
 const SkillsScreen = () => {
+    const skills = [
+        {
+            skillName: "React",
+            level: 5,
+        },
+        {
+            skillName: "Node",
+            level: 3,
+        },
+        {
+            skillName: "CSS",
+            level: 5,
+        },
+        {
+            skillName: "HTML",
+            level: 5,
+        },
+        {
+            skillName: "JavaScript",
+            level: 5,
+        },
+        {
+            skillName: "TypeScript",
+            level: 5,
+        },
+    ] as SkillItemProps[];
+
+    const navigation = useNavigation<NavigationProps>();
+
+    
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#000"  }}>
-            <Text style={{ color: "#fff"}}>
-                Skills
-            </Text>
+        <View style={styles.container}>
+            <Pressable onPress={() => navigation.goBack()} style={styles.backButtonContainer}>
+                <Feather name={"chevron-left"} size={24} color="white" />
+            </Pressable>
+
+            <Header title="My skills" />
+
+            <SkillsList skills={skills} />
+
+            <Button
+                text="Talk to me"
+                onPress={() => {}}
+            />
+
+            <StatusBar style="light" />
         </View>
     )
 }
